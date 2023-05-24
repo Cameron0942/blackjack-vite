80ae13a75cb6dc053e99cdeb36eec66260968b75
Cameron B <Cameron0942@users.noreply.github.com>
2023-05-24 00:31:42 -0700

    change config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://cameron0942.github.io/blackjack-vite/',
+  base: '/blackjack-vite/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx'],
-  }
-})
+  },
+  build: {
+    outDir: 'docs',
+  },
+});
