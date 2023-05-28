7691abb8d0086e3ee8c2a95ee8a0dfd05496f41f
Cameron B <Cameron0942@users.noreply.github.com>
2023-05-27 23:24:21 -0700

    Changed font and small navbar change

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -5,9 +5,9 @@ const Title = () => {
 
     return(
         <div style={{textAlign: 'center', marginBottom: '2vh'}}>
-            <img src={TitleSprite} alt="Blackjack Logo" style={{maxHeight: '100px'}} />
-            <span className="title">  Blackjack (21)</span>
-            <img src={TitleSprite} alt="Blackjack Logo" style={{maxHeight: '100px'}} />
+            <img src={TitleSprite} alt="Blackjack Logo" style={{maxHeight: '100px', marginRight: 25}} />
+            <span className="title">BLACKJACK</span>
+            <img src={TitleSprite} alt="Blackjack Logo" style={{maxHeight: '100px', marginLeft: 25}} />
         </div>
         
     );
